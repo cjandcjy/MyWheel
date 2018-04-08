33df71243f40d1a78e0b433571f2b658cca69e2a
cjandcjy <317672398@qq.com>
2018-04-08 10:21:20 +0800

    update

---
+++ Problem/index6.js
@@ -0,0 +1,21 @@
+/**js 原型链 */
+
+
+var a = {a: 1}; 
+// a ---> Object.prototype ---> null
+
+var b = Object.create(a);
+// b ---> a ---> Object.prototype ---> null
+console.log(b.a); // 1 (继承而来)
+
+var c = Object.create(b);
+// c ---> b ---> a ---> Object.prototype ---> null
+
+var b = ["a","b"]
+// b ---> Array.prototype ---> Object.prototype ---> null 
+
+var c = function(){
+
+}
+// c --->Function.prototype ---> Object.prototype ---> null
+
